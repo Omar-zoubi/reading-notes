f609cebc470742e60d41f3d0388a246ae3af0071
Omar-zoubi <o.fandi02@gmail.com>
2021-01-27 15:38:53 +0200

    Javasc

---
+++ app.js
@@ -0,0 +1,10 @@
+var name = prompt("Enter yor name ","name")
+alert("Welcome to my Website "+name)
+var x = confirm("Do you want to vist us again ")
+
+var pasword = "0000"
+var uinput 
+while (uinput !=pasword) {
+    uinput=prompt("Enter the password ")
+    
+}
