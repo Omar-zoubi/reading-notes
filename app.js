f1dee90ecf86caab61d0efe45a06fb53e5fb4d76
Omar-zoubi <o.fandi02@gmail.com>
2021-01-28 04:15:45 +0200

    enhancing

--- app.js
+++ app.js
@@ -8,4 +8,13 @@ while (uinput !=pasword) {
     uinput=prompt("Enter the password ")
     
 }
+ 
+var rate  = prompt("Enter yor rate ","1 star ,tow star ,three star four star  ")
+for(var i =0 ;i <rate ;i++)
+{
+   var star = star + '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Golden_star.svg/1200px-Golden_star.svg.png">'
+    console.log(star)
+}
 
+var image = '<img src="https://wholefully.com/wp-content/uploads/2019/04/no-banana-smoothies_972x1272_acf_cropped-735x962.jpg">'
+ console.log(image) 
